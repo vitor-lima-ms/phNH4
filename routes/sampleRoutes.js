8f906b10b8315b57af5b79d9e0271af7106dbe0e
Vitor Lima <viitorlima2000@gmail.com>
2025-06-24 16:48:01 -0300

    Remove authentication middleware from sample routes
    Eliminated the use of the checkAuth middleware from all sample-related routes, making them accessible without authentication. This change may be for development, testing, or to allow public access to these endpoints.

--- routes/sampleRoutes.js
+++ routes/sampleRoutes.js
@@ -3,41 +3,24 @@ const router = express.Router();
 const multer = require("multer");
 
 const SampleController = require("../controllers/SampleController");
-const Sample = require("../models/Sample");
 
-const checkAuth = require("../helpers/auth").checkAuth;
+router.get("/list", SampleController.listSamples);
 
-router.get("/list", checkAuth, SampleController.listSamples);
+router.post("/delete", SampleController.deleteSample);
 
-router.post("/delete", checkAuth, SampleController.deleteSample);
-
-router.get("/edit/:id", checkAuth, SampleController.editSampleGet);
-router.post("/edit", checkAuth, SampleController.editSamplePost);
+router.get("/edit/:id", SampleController.editSampleGet);
+router.post("/edit", SampleController.editSamplePost);
 
 const upload = multer({ dest: "uploads" });
-router.get("/import", checkAuth, SampleController.importCsvGet);
+router.get("/import", SampleController.importCsvGet);
 router.post(
   "/import",
-  checkAuth,
+
   upload.single("csvFile"),
   SampleController.importCsvPost
 );
 
-router.get("/delete-import", checkAuth, SampleController.deleteImportGet);
-router.post("/delete-import", checkAuth, SampleController.deleteImportPost);
-
-// router.get("/step1/:id", checkAuth, SampleController.step1Get);
-// router.post("/step1/:id", checkAuth, SampleController.step1Post);
-
-// router.get("/step2/:id", checkAuth, SampleController.step2Get);
-// router.post("/step2/:id", checkAuth, SampleController.step2Post);
-
-// router.get("/step3/:id", checkAuth, SampleController.step3Get);
-// router.post("/step3/:id", checkAuth, SampleController.step3Post);
-
-// router.get("/step4/:id", checkAuth, SampleController.step4Get);
-// router.post("/step4/:id", checkAuth, SampleController.step4Post);
-
-router.post("/classification", checkAuth, SampleController.classification);
+router.get("/delete-import", SampleController.deleteImportGet);
+router.post("/delete-import", SampleController.deleteImportPost);
 
 module.exports = router;
